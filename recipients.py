8fb487e5827e4416ffbcd066f1c5bca68522eb7c
Alan <alanton@berkeley.edu>
2020-06-01 19:04:10 -0700

    added san jose councilmembers

--- recipients.py
+++ recipients.py
@@ -9,6 +9,17 @@ county_list = [
         ( "Councilmember Paul Krekorian", "LA", "councilmember.Krekorian@lacity.org"),
         ( "Councilmember Bob Blumenfield", "LA", "councilmember.blumenfield@lacity.org"),
         ( "Councilmember David E. Ryu", "LA", "david.ryu@lacity.org"),
+        ( "Mayor Sam Liccardo", "SJ", "mayoremail@sanjoseca.gov"),
+        ( "Vice Mayor Charles Jones", "SJ", "District1@sanjoseca.gov"),
+        ( "Councilmember Sergio Jimenez", "SJ", "District2@sanjoseca.gov"),
+        ( "Councilmember Raul Peralez", "SJ", "District3@sanjoseca.gov"),
+        ( "Councilmember Lan Diep", "SJ", "District4@sanjoseca.gov"),
+        ( "Councilmember Magdalena Carrasco", "SJ", "District5@sanjoseca.gov"),
+        ( "Councilmember Devora Davis", "SJ", "District6@sanjoseca.gov"),
+        ( "Councilmember Maya Esparza", "SJ", "District7@sanjoseca.gov"),
+        ( "Councilmember Sylvia Arenas", "SJ", "District8@sanjoseca.gov"),
+        ( "Councilmember Pam Foley", "SJ", "District9@sanjoseca.gov"),
+        ( "Councilmember Johnny Khamis", "SJ", "District10@sanjoseca.gov"),
     ]
 
 def gen_recipients(location):
