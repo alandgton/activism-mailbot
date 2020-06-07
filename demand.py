4c500f341cde1002332ff2be7b75663de903291a
Alan <alanton@berkeley.edu>
2020-06-06 20:42:16 -0700

    split message into chunks

--- demand.py
+++ demand.py
@@ -1,1 +1,19 @@
 import messages, random
+
+"""
+def ___________():
+My name is [Name], and I am reaching out to demand that the Mayor and City Council redirect money away from the police department and invest into community resources. It has been proven time and time again that police brutality is a systemic issue and has racially targeted Black and Brown people for hundreds of years.
+
+def ___________():
+This is unacceptable. At this time Black people and their allies are demanding systemic change to policing, and in light of COVID19, we need to restructure funding to focus on preserving community resources instead of excessively bolstering the police.
+
+def ___________():
+This city’s funds should be redirected into programs that support the needs of our community: quality housing for all, educational enrichment, good jobs, free and quality public transportation, universal access to healthy food, climate justice, mental healthcare, and small business resources amongst others.
+
+def ___________():
+As elected officials, you have a duty to listen to your community, and focus on benefiting all of your constituents rather than overprotecting the police. We will continue to make our voice heard. 
+
+// This is done already in messages.gen_closing()
+Sincerely, 
+[Your Full Name Here]
+"""
